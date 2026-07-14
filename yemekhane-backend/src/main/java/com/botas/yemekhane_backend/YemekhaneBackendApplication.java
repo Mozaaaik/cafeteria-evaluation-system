a3cdac0bace473fb72f25571d5a98fe842639eea
Furkan Elidolu <furkanelidolu71@gmail.com>
2026-07-14 22:11:34 +0300

    Backend bağımlılıkları eklendi MongoDB veritabanı bağlantısı kuruldu health endpointi ile bağlantı test edildi

---
+++ yemekhane-backend/src/main/java/com/botas/yemekhane_backend/YemekhaneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.botas.yemekhane_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YemekhaneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YemekhaneBackendApplication.class, args);
+	}
+
+}
---
+++ yemekhane-backend/src/test/java/com/botas/yemekhane_backend/YemekhaneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botas.yemekhane_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YemekhaneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
